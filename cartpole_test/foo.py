6128c5f4b24eba0f2a9698fb16b2de5f70e19d0a
ds12a <dshen1248@gmail.com>
2026-04-22 23:55:50 -0400

    cleaning
    Co-authored-by: Aaron Chen <quickgivemememes@gmail.com>

--- cartpole_test/foo.py
+++ cartpole_test/foo.py
@@ -1,10 +1,11 @@
 from cartpole_mod import CartPoleEnv
 import casadi as ca
 import numpy as np
+from gymnasium.utils.play import play
 
 # Euler / direct shooting MPC
 
-# Constat
+# Constants
 POLE_LEN = 0.5
 FORCE = 10
 
@@ -19,8 +20,17 @@ opti = ca.Opti()
 env = CartPoleEnv(render_mode="human")
 
 
-
 env.reset()
+# play(
+#     env,
+#     keys_to_action={
+#         (ord("a"),): 0,
+#         (ord("d"),): 1,
+#     },
+#     noop=-1,
+#     zoom=1,
+# )
+
 
 def gen_dynamics(dt):
     x = ca.SX.sym("x", 4)
@@ -32,7 +42,7 @@ def gen_dynamics(dt):
     costheta = ca.cos(theta)
     sintheta = ca.sin(theta)
 
- 
+
     temp = (
         u + POLE_LEN * (theta_dot * theta_dot) * sintheta
     ) / TOTAL_MASS
@@ -46,9 +56,9 @@ def gen_dynamics(dt):
     d_x_c_next = d_x_c + dt * xacc
     theta_next = theta + dt * theta_dot
     theta_dot_next = theta_dot + dt * thetaacc
-    
+
     x_next = ca.vertcat(x_c_next, d_x_c_next, theta_next, theta_dot_next)
-    
+
     dynam_f = ca.Function("dynamics", [x, u], [x_next])
     return dynam_f
 
@@ -57,7 +67,7 @@ def setup_mpc(x0):
     dt = 0.02 * 2
     x_threshold = 2.4
     n = 20
-    u = opti.variable(n) # 1 sec lookahead
+    u = opti.variable(n)
 
     apply_dynamics = gen_dynamics(dt)
 
@@ -68,16 +78,16 @@ def setup_mpc(x0):
     for i in range(n):
         x = apply_dynamics(x, u[i])
 
-        J += x[2] ** 2 + x[0]**2 
-        
+        J += x[2] ** 2 + x[0]**2
+
         # J += x[3]**2
         opti.subject_to(opti.bounded(-x_threshold, x[0], x_threshold))
         opti.subject_to(opti.bounded(-10, u[i], 10))
-        
+
     opti.minimize(J)
 
     return u
-    
+
 
 x0 = opti.parameter(4) # init state [cart pos, cart vel, pole angle, pole angle vel]
 u = setup_mpc(x0)
@@ -102,24 +112,18 @@ ipopt_settings = {
 }
 opti.solver("ipopt", ipopt_settings)
 
-i = 0
 opti.set_value(x0, ca.vertcat(0, 0, ca.pi, 0))
 
 while True:
-    print(f"Solving with {opti.nx} variables.")
-    # try:
+    # print(f"Solving with {opti.nx} variables.")
+
     sol = opti.solve()
     stats = sol.stats()
-    print(f"Solve iteration succeeded in {stats['iter_count']} iterations")
-    # except:
-    #     sol = opti.debug
-    #     stats = sol.stats()
-    #     print(f"Solve iteration failed after {stats['iter_count']} iteration...")
+    # print(f"Solve iteration succeeded in {stats['iter_count']} iterations")
 
 
     force = sol.value(u)[0]
-    # force = sol.value(u)
-    print(force)
+    # print(force)
 
     # for f in force:
     #     observation, reward, terminated, truncated, info = env.step(1 if f > 0 else 0)
@@ -128,10 +132,8 @@ while True:
     observation, reward, terminated, truncated, info = env.step(float(force))
 
     opti.set_value(x0, ca.vertcat(*observation))
-    
-    if terminated: break
 
-    # i += 1
+    if terminated: break
 
 
 env.close()
